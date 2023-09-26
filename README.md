2a2d3ebe5e5cf59e620352fca0e11153fc87c0b6
Vikash <vikashkaushik910@gmail.com>
2023-09-26 16:19:05 +0530

    Day 02 art Introduction

--- README.md
+++ README.md
@@ -5,4 +5,16 @@ Creating first flutter Project
 ## Getting Started
 
    Day 01: Going to  lib > "main.dart" file and created a simple UI.
+                Step 1 ---> Step 5
+
+   Day 02: Dart Language introduction
+            Main.dart
+                Step 6 ---> 
+            Multiple pages banayenge taaki main.dart file complex na ho jaye
+                step 7 ---> home_page.dart
+                Step 8 Replacing "material" widget to "Scaffold" widget
    
+                       Widget      Scaffold 
+                       Childs      appBar(header)
+                                    body (body)
+                                    bottomSheet()/bottomNavigationBar() (footar)
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 
 // Step 1: Lets import material.dat (its a library
 import 'package:flutter/material.dart';
-
+import './HomePage.dart';
 // Step 2: we start with creating main method , main method is the entry point of flutter project.
 void main(){
   // step 3 : runApp method define karenge and argument denge app ka naam
@@ -15,20 +15,15 @@ class FlutterBootcamp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    // Step 6.1: initalising variable DAY NUMBER  dynamically
+
     return
 // Step 5: Child me ek text widget denge , Container ko use karne ke liye koi App deni hogi like- widgetApp,MaterialApp and cuppertino app
 //     yaha material app use kar rahe hai
       MaterialApp(
-        home:Material(
+        home:HomePage(),
+        );
 
-
-      child: Center(
-
-      child: Container(
-          child: Text("Welcome to FlutterBootcamp")
-        ),
-      ),
-        ),
-      );
   }
 }
