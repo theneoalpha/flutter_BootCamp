39e88addfd1f43cdd1cf03bef91835aaa419836e
Vikash <vikashkaushik910@gmail.com>
2023-10-06 00:47:52 +0530

    Day 06 StateFulWidget and Animated Container/Button

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -3,9 +3,19 @@ import './utlis/routes.dart';
 import 'package:flutter/material.dart';
 
 import './HomePage.dart';
-class LoginPage extends StatelessWidget {
+//Step 18.1 Changing to statefulWidget
+class LoginPage extends StatefulWidget {
 
 
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  //Step 18.2 String define karenge 'name'
+  String name = " ";
+  //Step 19.6
+  bool changeButton = false;
   @override
   Widget build(BuildContext context) {
     return Material(
@@ -24,8 +34,8 @@ class LoginPage extends StatelessWidget {
             Image.asset("assets/images/developer.png",fit: BoxFit.cover,),
 
             //Step 12.4 :  Image ke niche text element denge
-
-            Text("Welcome"),
+            //Step 18.3 : using name string me text field
+            Text("Welcome $name"),
             // step 13.1 : Adding TextField element
             SizedBox(
               height: 20.0,
@@ -43,6 +53,12 @@ class LoginPage extends StatelessWidget {
                       hintText: "Enter Username",
                       labelText: "username",
                     ),
+                    //Step 18.4 onChanged fuction
+                    onChanged: (value){
+                      name = value;
+                      setState(() {
+                      });
+                    },
                   ),
                   //Step 13.3 Same password ke liye textfield banayenge
                   TextField(
@@ -54,13 +70,44 @@ class LoginPage extends StatelessWidget {
                     ),
                   ),
                   //Step 15: Adding Elevated button
-                  ElevatedButton(
-                    child: Text("Login"),
-                    style: TextButton.styleFrom(minimumSize: Size(150, 40)),
-                    onPressed: () {
-                      //Step 17.3: accessing our MyRoutes routes
+                  //Step 19.1 : Commenting elevated button
+                  // ElevatedButton(
+                  //   child: Text("Login"),
+                  //   style: TextButton.styleFrom(minimumSize: Size(150, 40)),
+                  //   onPressed: () {
+                  //     //Step 17.3: accessing our MyRoutes routes
+                  //     Navigator.pushNamed(context, MyRoutes.homeRoute);
+                  //   },
+                  // )
+                  //step 19.2 Container banayenge
+                  //Step 19.4 Inkwell me wrap karenge Container ko
+                  InkWell(
+                    //Step 19.7
+                    //Step 19.9 : Async and await
+                    onTap: () async{
+                      setState(() {
+                        changeButton = true;
+                      });
+                      await Future.delayed(Duration(seconds: 1));
                       Navigator.pushNamed(context, MyRoutes.homeRoute);
                     },
+                    //Step 19.5 Container to AnimatedContainer
+                    child: AnimatedContainer(
+                      duration: Duration(seconds: 1),
+                      width: 100,
+                      height: 50,
+
+                      alignment: Alignment.center,
+                      //Step 19.8 : doneIcon add karenge onTap me
+                      child: changeButton?Icon(Icons.done): Text("Login",
+                        style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold)
+                        ,),
+                      //Step 19.3 : Box decoration add
+                      decoration: BoxDecoration(
+                        color: Colors.deepPurple,
+                        borderRadius: BorderRadius.circular(10)
+                      ),
+                    ),
                   )
                 ],
               ),
